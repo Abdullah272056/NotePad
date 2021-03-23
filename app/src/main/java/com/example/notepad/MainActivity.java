5b72c52dcc54ece25d66d7e269ae0eee60756a05
Abdullah272056 <abdullah272056@gmail.com>
2021-03-23 20:43:17 +0600

    view finding

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    Button saveButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.editTextId);
+        saveButton=findViewById(R.id.saveButtonId);
+
+
+        
+
+
+
     }
 }
