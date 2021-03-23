45349ca3318da99cb1ad7c9c35e06d759c6979cd
Abdullah272056 <abdullah272056@gmail.com>
2021-03-23 20:53:06 +0600

    add static variable and create Table Query

--- app/src/main/java/com/example/notepad/Constants.java
+++ app/src/main/java/com/example/notepad/Constants.java
@@ -2,5 +2,18 @@ package com.example.notepad;
 
 public class Constants {
 
+    public  static final String DATABASE_NAME="NotePad.db";
+    public  static final int DATABASE_VERSION=1;
+    public  static final String TABLE_NAME="NotePad";
+
+    public  static final String COLUMN_ID="_id";
+    public  static final String COLUMN_INPUT_TEXT="InputValue";
+
+
+
+    public static final String CREATE_TABLE  = " CREATE TABLE "+TABLE_NAME+"("
+            +COLUMN_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "
+            +COLUMN_INPUT_TEXT+" TEXT "
+            +")";
 
 }
