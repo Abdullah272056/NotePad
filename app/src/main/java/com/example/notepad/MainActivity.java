fbb7c99f718549e300963cf52f5490d8da724b16
Abdullah272056 <abdullah272056@gmail.com>
2021-03-23 21:19:02 +0600

    data save function

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -24,7 +25,19 @@ public class MainActivity extends AppCompatActivity {
         dataBaseHelper=new DataBaseHelper(MainActivity.this);
         dataBaseHelper.getWritableDatabase();
 
-        
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String  inputValue=editText.getText().toString();
+
+                long id=dataBaseHelper.insertData(new MyDataType(inputValue));
+                if (id>0){
+                    Toast.makeText(MainActivity.this, "Successfully save", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
 
 
